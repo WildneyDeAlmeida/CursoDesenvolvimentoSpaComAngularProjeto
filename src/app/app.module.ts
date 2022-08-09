49ce334b13addf061e0cd3f44bdeb3918c0127b3
Wildney de Almeida Junior <103117531+WildneyDeAlmeida@users.noreply.github.com>
2022-08-09 16:19:41 -0300

    Separando o conteudo principal em 3 componentes: Menu, Home e footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [          // componentes
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [               // modulos 
+    BrowserModule,
+    
   ],
-  providers: [],
+  providers: [             // serviços
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
